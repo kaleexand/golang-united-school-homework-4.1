95e37437234cd3f2a7ef68606a63dd12dfa5e28a
kaleexand <kalexand@student.21-school.ru>
2022-06-17 22:41:11 +0300

    first version

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,20 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	for _, v := range input {
+		output = string(v) + output
+	}
 	return output
 }
+
+//
+//func main() {
+//	t3 := "Hello world!"
+//	res := ReverseString(t3)
+//	fmt.Println(res)
+//}
+
+//
+//input: Hello world!
+//
+//output: !dlrow olleH
